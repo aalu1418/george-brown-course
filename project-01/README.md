3faaec4f22547e7f497e2315fa3add1e64f3e329
aalu1418 <aalu1418@gmail.com>
2019-11-21 12:09:37 -0500

    project start & basic react framework - no states setup

--- project-01/README.md
+++ project-01/README.md
@@ -1,5 +1,59 @@
-1. Clone this repo
-1. `cd` into this directory
-1. Run `npm install`
-1. Run `npm start`
-1. Go into `src/App.js` to do the project
+# Project: Checkout Form
+### Submission Details
+Aaron Lu - 101278524  
+Deployed Website:  https://gb-fullstackiii-project.web.app/  
+Github Repository: https://github.com/aalu1418/george-brown-course/tree/master/project-01
+
+###### Included Technologies
+- React
+- Firebase (Firestore)
+- npm
+---
+
+### Useful Links
+- [Starting Firebase & React](https://www.youtube.com/watch?v=mwNATxfUsgI)
+
+### Task list
+- [ ] Create basic form with 3 sections in React (using states to store info)
+- [ ] Hide each form until previous section completed
+- [ ] Include Continue/Submit button functionality
+- [ ] Network offline handling
+- [ ] Integrate with Firestore
+  - [ ] Setup rules to allow for writing by anyone, but not reading
+  - [ ] Show submit loading state & completed state for submit button
+- [ ] Style with CSS - Bootstrap?
+
+---
+### Specifications
+Link for [Full Details](https://www.notion.so/Project-Checkout-Form-cc2deee7fee94165a5678ba5958242a7)
+- Use Firestore as your database.
+- Use React as your UI library.
+- Apply your own styling with CSS.
+- Create a form consisting of 3 sections:
+    1. Basic Info:
+        1. Text Input: First Name
+        2. Text Input: Last Name
+        3. Dropdown: Diet Restriction  
+            Options: None, Vegan, Vegetarian, Halal/Kosher
+    2. Address Info:
+        1. Text Input: City
+        2. Dropdown: Province
+    3. Payment Info:
+        1. Radio: Bitcoin | PayPal | Credit Card
+        2. Checkbox: Agree to Terms & Conditions
+- Hide each form section until the previous section is completed.
+- Save all the data when the last section is completed.
+- Show the loading state when saving the data.
+- Disable the button and show a warning when there is no network connection.
+- Disable the button when a section is not complete.
+
+### Submission Details
+Use the `project-01` directory in [github.com/amsul/george-brown-course](https://github.com/amsul/george-brown-course) as your starting point.
+
+Once your project is complete:
+
+1. Add a list of your team members (name & ID) in the `README.md` file.
+2. ZIP up the `project-01` directory.
+3. Upload the ZIP file on Blackboard.
+
+The ZIP file should contain all the files necessary to simply unzip and be able to run the project with `npm start`.
--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,17 +1,145 @@
 import './App.css'
 import React from 'react'
 import FormTitle from './components/FormTitle'
+import FormField from './components/FormField'
+import FormFieldHeading from './components/FormFieldHeading'
+import FormLabel from './components/FormLabel'
+import FormTextInput from './components/FormTextInput'
+import FormSubmit from './components/FormSubmit'
 
 export default function App() {
+  const info_basic = (
+    <FormSection title='Basic Information'>
+      <FormFieldTextInput
+        label='First Name'
+        placeholder='Input your first name'
+      ></FormFieldTextInput>
+      <FormFieldTextInput
+        label='Last Name'
+        placeholder='Input your last name'
+      ></FormFieldTextInput>
+      <FormFieldDropDown
+        label='Diet Restriction'
+        list={['None', 'Vegan', 'Vegetarian', 'Halal/Kosher']}
+      ></FormFieldDropDown>
+    </FormSection>
+  )
+
+  const info_address = (
+    <FormSection title='Address Information'>
+      <FormFieldTextInput
+        label='City'
+        placeholder='Input your city'
+      ></FormFieldTextInput>
+      <FormFieldDropDown
+        label='Province'
+        list={[
+          'Ontario',
+          'Alberta',
+          'British Columbia',
+          'Manitoba',
+          'New Brunswick',
+          'Newfoundland and Labrador',
+          'Nova Scotia',
+          'Prince Edward Island',
+          'Quebec',
+          'Saskatchewan',
+          'Northwest Territories',
+          'Nunavut',
+          'Yukon',
+        ]}
+      ></FormFieldDropDown>
+    </FormSection>
+  )
+
+  const info_payment = (
+    <FormSection title='Payment Information'>
+      <FormFieldRadioButtons
+        label='Payment Method'
+        list={['Bitcoin', 'PayPal', 'Credit Card']}
+      ></FormFieldRadioButtons>
+      <FormFieldCheckbox label='I agree to the Terms and Conditions'></FormFieldCheckbox>
+    </FormSection>
+  )
+
   return (
     <div className='App'>
       <div className='App-Content'>
         <div>
           <FormTitle>Checkout</FormTitle>
-
-          {/* Put your solution here 👇 */}
+          {[info_basic, info_address, info_payment]}
         </div>
       </div>
     </div>
+  )
+}
+
+const FormSection = ({ title, children }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      {children}
+      <FormSubmit isComplete={true} completeText='Continue'></FormSubmit>
+    </section>
+  )
+}
+
+const FormFieldFrame = ({ label, children }) => {
+  return (
+    <FormField>
+      <FormFieldHeading>
+        <FormLabel text={label}></FormLabel>
+      </FormFieldHeading>
+      {children}
+    </FormField>
+  )
+}
+
+const FormFieldTextInput = ({ label, placeholder }) => {
+  return (
+    <FormFieldFrame label={label}>
+      <FormTextInput placeholder={placeholder}></FormTextInput>
+    </FormFieldFrame>
+  )
+}
+
+const FormFieldDropDown = ({ label, setValue, list }) => {
+  const onChange = event => setValue(event.target.value)
+  return (
+    <FormFieldFrame label={label}>
+      <select onChange={onChange}>
+        {list.map(value => (
+          <option value={value}>{value}</option>
+        ))}
+      </select>
+    </FormFieldFrame>
+  )
+}
+
+const FormFieldRadioButtons = ({ label, list }) => {
+  return (
+    <FormFieldFrame label={label}>
+      {list.map(element => {
+        return (
+          <div>
+            <label>
+              <input type='radio' value={element} />
+              {element}
+            </label>
+          </div>
+        )
+      })}
+    </FormFieldFrame>
+  )
+}
+
+const FormFieldCheckbox = ({ label }) => {
+  return (
+    <FormFieldFrame>
+      <label>
+        <input type='checkbox' />
+        {label}
+      </label>
+    </FormFieldFrame>
   )
 }
