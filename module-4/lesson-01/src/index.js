fa4cc376e09b4c2bd5bb5dc39218d23690c12628
aalu1418 <aalu1418@gmail.com>
2019-11-27 08:13:39 -0500

    exercise 2 progress

--- module-4/lesson-01/src/index.js
+++ module-4/lesson-01/src/index.js
@@ -6,13 +6,13 @@ import * as serviceWorker from './serviceWorker'
 // import App from './App'
 // ReactDOM.render(<App />, document.getElementById('root'))
 
-// 👉 Exercise 1
-import Exercise1 from './Exercise1'
-ReactDOM.render(<Exercise1 />, document.getElementById('root'))
+// // 👉 Exercise 1
+// import Exercise1 from './Exercise1'
+// ReactDOM.render(<Exercise1 />, document.getElementById('root'))
 
 // 👉 Exercise 2
-// import Exercise2 from './__Exercise2__SOLUTION'
-// ReactDOM.render(<Exercise2 />, document.getElementById('root'))
+import Exercise2 from './Exercise2'
+ReactDOM.render(<Exercise2 />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
