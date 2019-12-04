63830bad6cb3fa8e1422a9cf366374f052c17d03
aalu1418 <aalu1418@gmail.com>
2019-12-03 22:07:11 -0500

    finished lesson assignment

--- module-4/lesson-03/src/App.js
+++ module-4/lesson-03/src/App.js
@@ -6,7 +6,21 @@ import Skeleton from '@material-ui/lab/Skeleton'
 import Button from '@material-ui/core/Button'
 import { BrowserRouter, Switch, Route, Link, useParams } from 'react-router-dom'
 
+import firebase from 'firebase'
+
 import fetchData from './utils/fetchData'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjMsOoWsy6MkmsTLZ_ZHeXL1wtt-7J7fU',
+  authDomain: 'gb-fullstackiv-lesson03.firebaseapp.com',
+  databaseURL: 'https://gb-fullstackiv-lesson03.firebaseio.com',
+  projectId: 'gb-fullstackiv-lesson03',
+  storageBucket: 'gb-fullstackiv-lesson03.appspot.com',
+  messagingSenderId: '682564674884',
+  appId: '1:682564674884:web:9926b7eef708990fcf6311',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const RECOMMENDED_CATS_URL =
   'https://api.thecatapi.com/v1/images/search?limit=10'
@@ -39,6 +53,7 @@ export default function App() {
 function HomePage() {
   return (
     <div>
+      <FeaturedCatsGrid />
       <FavoritedCatsRow />
       <RecommendedCatsGrid />
     </div>
@@ -220,4 +235,51 @@ function CatFavoriteButton({ cat }) {
 
 function getFavoriteCatsFromStorage() {
   return JSON.parse(window.localStorage.getItem('favorite-cats')) || []
+}
+
+function FeaturedCatsGrid() {
+  const [featuredCatIds, setFeaturedCatIds] = React.useState(null)
+  const [featuredCats, setFeaturedCats] = React.useState([])
+
+  React.useEffect(() => {
+    const db = firebase.firestore()
+    const featuredRef = db.collection('featured').doc('featured')
+    featuredRef
+      .get()
+      .then(doc => {
+        const catObj = doc.data()
+        setFeaturedCatIds(catObj.featured)
+      })
+      .catch(function(error) {
+        console.log('Error getting document:', error)
+      })
+  }, [])
+
+  React.useEffect(() => {
+    if (featuredCatIds == null || featuredCatIds.length === 0) {
+      return
+    }
+    Promise.all(
+      featuredCatIds.map(catId => fetchData(CAT_URL + catId)),
+    ).then(result => setFeaturedCats(result))
+  }, [featuredCatIds])
+
+  if (featuredCats.length === 0 ) {
+    return (
+      <div className='Loader'>
+        <CircularProgress />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Featured</h1>
+      <Grid container>
+        {featuredCats.map(cat => (
+          <CatItem key={cat.id} cat={cat} />
+        ))}
+      </Grid>
+    </div>
+  )
 }
