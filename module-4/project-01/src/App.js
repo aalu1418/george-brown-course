42b5e28e58838e4846a49a3bbe4a2b70742b5564
aalu1418 <aalu1418@gmail.com>
2019-12-12 00:54:27 -0500

    finished react form functionality TODO: integrate with firebase/firestore + metamask, and CSS styling

--- module-4/project-01/src/App.js
+++ module-4/project-01/src/App.js
@@ -43,8 +43,20 @@ import {
 const NETWORK = 'goerli'
 
 export default function App() {
-  const [state, setState] = React.useState({})
-  const userData = { state, setState }
+  const [values, setValues] = React.useState({
+    username: localStorage.getItem('m4p1-username') || '',
+    candidate: '',
+    happiness: '',
+    birthday: null,
+    province: '',
+    temperature: 20,
+    "donate-candidate" : "",
+    "donate-charity" : "",
+  })
+  const set = event => {
+    setValues({ ...values, [event.target.name]: event.target.value.trim() })
+  }
+  const userData = { values, set, setValues }
 
   return (
     <BrowserRouter>
@@ -52,24 +64,54 @@ export default function App() {
         <div className='App'>
           <div className='App-Content'>
             <Solution />
-            <Route exact path='/voting/1'>
-              <Part1 />
-            </Route>
-            <Route exact path='/voting/2'>
-              <Part2 />
-            </Route>
-            <Route exact path='/voting/3'>
-              <Part3 />
-            </Route>
-            <Route exact path='/voting/summary'>
-              <Summary />
-            </Route>
-            <Route exact path='/results'>
-              <Results />
-            </Route>
-            <Route exact path='/'>
-              <LoginPage />
-            </Route>
+            <CheckedRoute
+              exact
+              path='/voting/1'
+              condition={!userData.values.username}
+              render={() => <Part1 state={userData} />}
+            />
+            <CheckedRoute
+              exact
+              path='/voting/2'
+              condition={
+                !userData.values.username ||
+                !userData.values.candidate ||
+                !userData.values.happiness
+              }
+              render={() => <Part2 state={userData} />}
+            />
+            <CheckedRoute
+              exact
+              path='/voting/3'
+              condition={
+                !userData.values.username ||
+                !userData.values.birthday ||
+                !userData.values.province
+              }
+              render={() => <Part3 state={userData} />}
+            />
+            <CheckedRoute
+              exact
+              path='/voting/summary'
+              condition={
+                !userData.values.username ||
+                !userData.values.birthday ||
+                !userData.values.province
+              }
+              render={() => <Summary state={userData} />}
+            />
+            <CheckedRoute
+              exact
+              path='/results'
+              condition={!userData.values.username}
+              render={() => <Results />}
+            />
+            <CheckedRoute
+              exact
+              path='/'
+              condition={false}
+              render={() => <LoginPage state={userData} />}
+            />
           </div>
         </div>
       </MuiPickersUtilsProvider>
@@ -78,15 +120,24 @@ export default function App() {
 }
 
 function Solution() {
+  let location = useLocation()
+  const showBack = !['/', '/voting/1', '/voting/summary', '/results'].includes(
+    location.pathname,
+  )
+
   return (
     <div>
       <Grid container={true} justify='space-between'>
         <Typography component='h1' gutterBottom={true}>
           Cast Your Vote
         </Typography>
-        <Link to='/' component={RouterLink}>
-          Back to start
-        </Link>
+        <div>
+          {showBack && (
+            <Link to='/' component={RouterLink}>
+              Back to start
+            </Link>
+          )}
+        </div>
       </Grid>
     </div>
   )
@@ -98,8 +149,13 @@ const PageContainer = ({
   continueTo,
   previousTo,
   continueDisable,
-  previousDisable,
+  previousDisable = false,
+  onContinueClick = () => null,
 }) => {
+  let location = useLocation()
+  const isSummary = location.pathname.split("/")[2] === "summary"
+  const submitButton = <span><DoneAllIcon /> Cast Votes</span>
+
   return (
     <Grid container direction='column' justify='center' alignItems='flex-start'>
       <Typography variant='h4'>{title}</Typography>
@@ -107,17 +163,28 @@ const PageContainer = ({
       <Grid
         style={{ width: '100%' }}
         container
-        direction='row'
+        direction={isSummary? 'column-reverse' : 'row'}
         justify='space-between'
       >
-        {previousTo && <Button to={previousTo} component={RouterLink}>
-          {' '}
-          Previous{' '}
-        </Button>}
-        {continueTo && <Button to={continueTo} component={RouterLink}>
-          {' '}
-          Continue{' '}
-        </Button>}
+        {previousTo && (
+          <Button
+            to={previousTo}
+            component={RouterLink}
+            disabled={previousDisable}
+          >
+            { isSummary? "Go Back" : "Previous"}
+          </Button>
+        )}
+        {continueTo && (
+          <Button
+            to={continueTo}
+            component={RouterLink}
+            disabled={continueDisable}
+            onClick={onContinueClick}
+          >
+            {isSummary ?  submitButton : "Continue"}
+          </Button>
+        )}
       </Grid>
     </Grid>
   )
@@ -132,22 +199,47 @@ const QuestionContainer = ({ label, children }) => {
   )
 }
 
-const LoginPage = () => {
+const LoginPage = ({ state }) => {
+  const writeToStorage = () => {
+    localStorage.setItem('m4p1-username', state.values.username)
+  }
+
   return (
-    <PageContainer continueTo="/voting/1">
+    <PageContainer
+      continueTo='/voting/1'
+      continueDisable={!state.values.username}
+      onContinueClick={writeToStorage}
+    >
+      {localStorage.getItem('m4p1-username') && <Redirect to='/voting/1' />}
       <QuestionContainer label='To begin your voting application, please enter a username:'>
-        <TextField variant='outlined' label='Username'></TextField>
+        <TextField
+          variant='outlined'
+          label='Username'
+          name='username'
+          value={state.values.username}
+          onChange={state.set}
+        />
       </QuestionContainer>
     </PageContainer>
   )
 }
 
-const Part1 = () => {
+const Part1 = ({ state }) => {
   return (
-    <PageContainer title='Part 1' continueTo="/voting/2" previousTo="/">
+    <PageContainer
+      title='Part 1'
+      continueTo='/voting/2'
+      previousTo='/'
+      previousDisable={true}
+      continueDisable={!state.values.candidate || !state.values.happiness}
+    >
       <QuestionContainer label='Who is your favorite candidate?'>
         <FormControl>
-          <RadioGroup name='candidate'>
+          <RadioGroup
+            name='candidate'
+            value={state.values.candidate}
+            onChange={state.set}
+          >
             {Object.keys(CANDIDATE_NAME).map(key => (
               <FormControlLabel
                 key={key}
@@ -161,7 +253,11 @@ const Part1 = () => {
       </QuestionContainer>
       <QuestionContainer label='How happy are you with the current progress?'>
         <FormControl>
-          <RadioGroup name='happiness'>
+          <RadioGroup
+            name='happiness'
+            value={state.values.happiness}
+            onChange={state.set}
+          >
             {Object.keys(HAPPINESS_LABEL).map(key => (
               <FormControlLabel
                 key={key}
@@ -177,22 +273,40 @@ const Part1 = () => {
   )
 }
 
-const Part2 = () => {
+const Part2 = ({ state }) => {
   return (
-    <PageContainer title='Part 2' continueTo="/voting/3" previousTo="/voting/2">
+    <PageContainer
+      title='Part 2'
+      continueTo='/voting/3'
+      previousTo='/voting/1'
+      continueDisable={!state.values.birthday || !state.values.province}
+    >
       <QuestionContainer label='When is your birthday?'>
         <KeyboardDatePicker
+          name='birthday'
           variant='inline'
           format='MM/dd/yyyy'
           margin='normal'
           KeyboardButtonProps={{
             'aria-label': 'change date',
           }}
+          value={state.values.birthday}
+          onChange={date =>
+            state.setValues({ ...state.values, birthday: date })
+          }
         />
       </QuestionContainer>
       <QuestionContainer label='Which province do you reside in?'>
         <FormControl variant='outlined' style={{ minWidth: '100%' }}>
-          <Select>
+          <Select
+            name='province'
+            value={state.values.province}
+            onChange={state.set}
+          >
+            <MenuItem key={''} value={''}>
+              {' '}
+              --{' '}
+            </MenuItem>
             {PROVINCES.map(province => (
               <MenuItem key={province.code} value={province.code}>
                 {province.name}
@@ -205,7 +319,7 @@ const Part2 = () => {
   )
 }
 
-const Part3 = () => {
+const Part3 = ({ state }) => {
   const marks = [
     {
       value: 0,
@@ -225,12 +339,19 @@ const Part3 = () => {
     },
   ]
   return (
-    <PageContainer title='Part 3' continueTo="/voting/summary" previousTo="/voting/2">
+    <PageContainer
+      title='Part 3'
+      continueTo='/voting/summary'
+      previousTo='/voting/2'
+    >
       <QuestionContainer label='What is your ideal room temperature?'>
         <Slider
-          defaultValue={20}
+          value={state.values.temperature}
+          onChange={(_, value) =>
+            state.setValues({ ...state.values, temperature: value })
+          }
           aria-labelledby='temperature-slider'
-          step={1}
+          step={2}
           valueLabelDisplay='auto'
           marks={marks}
         />
@@ -239,21 +360,47 @@ const Part3 = () => {
   )
 }
 
-const Summary = () => {
+const Summary = ({ state }) => {
+  const QuestionResponse = ({ text }) => {
+    return <Typography variant='body1'>{text}</Typography>
+  }
+
+  const birthdayParse = !state.values.birthday
+    ? ' '
+    : `${state.values.birthday.getMonth()}/${state.values.birthday.getDay()}/${state.values.birthday.getFullYear()}`
+
   return (
-    <PageContainer title='Summary' continueTo="/results" previousTo="/voting/3">
-      <QuestionContainer label='Who is your favorite candidate?'></QuestionContainer>
-      <QuestionContainer label='How happy are you with the current progress?'></QuestionContainer>
-      <QuestionContainer label='When is your birthday?'></QuestionContainer>
-      <QuestionContainer label='Which province do you reside in?'></QuestionContainer>
-      <QuestionContainer label='What is your ideal room temperature?'></QuestionContainer>
+    <PageContainer title='Summary' continueTo='/results' previousTo='/voting/3'>
+      <QuestionContainer label='Who is your favorite candidate?'>
+        <QuestionResponse text={CANDIDATE_NAME[state.values.candidate]} />
+      </QuestionContainer>
+      <QuestionContainer label='How happy are you with the current progress?'>
+        <QuestionResponse text={HAPPINESS_LABEL[state.values.happiness]} />
+      </QuestionContainer>
+      <QuestionContainer label='When is your birthday?'>
+        <QuestionResponse text={birthdayParse} />
+      </QuestionContainer>
+      <QuestionContainer label='Which province do you reside in?'>
+        <QuestionResponse text={PROVINCES[state.values.province]} />
+      </QuestionContainer>
+      <QuestionContainer label='What is your ideal room temperature?'>
+        <QuestionResponse text={`${state.values.temperature} ${'\u00b0'}C`} />
+      </QuestionContainer>
       <TextField
+        name="donate-candidate"
+        type="number"
         variant='outlined'
         label='Donate ETH to your candidate (optional)'
+        value={state.values["donate-candidate"]}
+        onChange={state.set}
       ></TextField>
       <TextField
+        name="donate-charity"
+        type="number"
         variant='outlined'
         label='Donate ETH to charity (optional)'
+        value={state.values["donate-charity"]}
+        onChange={state.set}
       ></TextField>
     </PageContainer>
   )
@@ -261,7 +408,7 @@ const Summary = () => {
 
 const Results = () => {
   return (
-    <PageContainer title='Summary'>
+    <PageContainer title='Results'>
       <QuestionContainer label='Favorite candidate:'></QuestionContainer>
       <QuestionContainer label='Progress:'></QuestionContainer>
       <QuestionContainer label='Age groups:'></QuestionContainer>
@@ -270,3 +417,16 @@ const Results = () => {
     </PageContainer>
   )
 }
+
+const conditionalRedirect = Component => {
+  return function NewComponent({ condition, ...props }) {
+    return (
+      <div>
+        {condition && <Redirect to='/' />}
+        <Component {...props} />
+      </div>
+    )
+  }
+}
+
+const CheckedRoute = conditionalRedirect(Route)
