7e4f8cf125bfaf1ecd7cd579f2d8096b4898c640
amsul <reach@amsul.ca>
2019-12-12 19:09:07 -0500

    Add Module 4 Lesson 6 files
    Signed-off-by: amsul <reach@amsul.ca>

---
+++ module-4/lesson-06/src/sendTransaction.js
@@ -0,0 +1,31 @@
+import { ethers } from 'ethers'
+
+const NETWORK = 'goerli'
+
+export default async function sendTransaction({ valueInEth, gas }) {
+  const accounts = await window.ethereum.enable()
+  console.log('Accounts found:', accounts)
+
+  const provider = ethers.getDefaultProvider(NETWORK)
+  const gasPrice = await provider.getGasPrice()
+
+  const transactionParameters = {
+    to: '0x67478a68098f052C3e3B609b521f88638bd3eaA5',
+    from: accounts[0],
+    gas: ethers.utils.hexlify(gas),
+    gasPrice: gasPrice.toHexString(),
+    value: valueInEth
+      ? ethers.utils.parseEther(valueInEth).toHexString()
+      : undefined,
+  }
+
+  console.log('Sending transaction with params:', transactionParameters)
+  const response = await window.ethereum.send('eth_sendTransaction', [
+    transactionParameters,
+  ])
+
+  console.log(
+    'Sent transaction: %o',
+    `https://${NETWORK}.etherscan.io/tx/${response.result}`,
+  )
+}
